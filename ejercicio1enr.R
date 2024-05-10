afe7eb49903aa5308fed8060102215f279f0bddc
Marco Arribas <marco.arribas@alumni.mondragon.edu>
2024-05-10 09:14:01 +0200

    cambiar titulo

---
+++ ejercicio1enr.R
@@ -0,0 +1,34 @@
+#1er EJERCICIO RETO 04 DATASCIENCE:
+library(readxl)
+library(dplyr)
+#como ya hemos generado un proyecto, ya estamos en el directorio en el que queremos trabajar
+getwd()
+
+#5.Carga del fichero y revisión de formatos:
+df<-read_excel("Datos//online_retail_II.xlsx")
+str(df)
+class(df$InvoiceDate)
+df$InvoiceDate<-as.Date(df$InvoiceDate)
+
+#6.Análisis básico de la información contenida en el fichero:
+summary(df)
+
+#7. Calcula cuantos pedidos distintos hay en el fichero:
+length(unique(df$Invoice))
+
+#10. Calcula el precio medio de producto por pedido:
+precio_medio_productos_por_pedido<-df %>% 
+  group_by(Invoice) %>% 
+  summarise(precio_medio=mean(Price))
+  
+#12. Calcula el numero de pedidos por pedido:
+numero_productos_por_pedido<-df %>% 
+  group_by(Invoice) %>% 
+  summarise(numero_productos=n())
+
+#13. Calcula el precio por pedido:
+precio_por_pedido<-df %>% 
+  group_by(Invoice) %>% 
+  summarise(precio=sum(Price))
+
+
